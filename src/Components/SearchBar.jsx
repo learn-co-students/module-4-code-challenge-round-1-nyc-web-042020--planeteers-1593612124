084f73a381c54248a7afcd77653ba49186cddc15
github username <36998937+katmaldon@users.noreply.github.com>
2020-07-01 11:38:02 -0400

    search bar working

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 class SearchBar extends React.Component {
 
-  render() {
-    return (
-      <div className="search">
+    render() {
+        return (
+            <div className="search">
+                <input
+                    type="text"
+                    placeholder={"Search for planeteers"}
+                    onChange={(e) => this.props.handlesearch(e.target.value)}
+                />
       </div>
     );
   }
